09452b79918b8c58a070b54456a87d9854b32821
youyf2 <842056007@qq.com>
2025-04-22 07:02:12 +0000

    clean ubuntu 22.04 debootstrap
    Signed-off-by: youyf2 <842056007@qq.com>

---
+++ ubuntu/ubuntu22.04/img/aarch64/README.md
@@ -0,0 +1,3 @@
+# debootstrap for jammy
+
+https://salsa.debian.org/installer-team/debootstrap/-/tree/master?ref_type=heads
---
+++ ubuntu/ubuntu22.04/img/aarch64/bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -x
+
+# 参数配置
+TARGET_DIR="./rootfs"
+MIRROR="https://mirrors.ustc.edu.cn/ubuntu-ports/"  # 使用国内镜像加速
+ARCH="arm64"
+RELEASE="jammy"
+EXT4_IMAGE="rootfs.ext4"
+SQUASHFS_IMAGE="rootfs.squashfs"
+COMPRESS_LEVEL=9  # 压缩级别 1-9
+INCLUDE_PACKAGES=$(tr '\n' ',' < packages.list | sed 's/,$//')
+
+#if [ -d rootfs ] ; then
+#	echo "find rootfs, do you want to remove and rebuild ?"
+#	exit 1
+#fi
+
+# 安装依赖
+apt-get update
+apt-get install -y debootstrap squashfs-tools e2fsprogs
+
+echo "extra package:${INCLUDE_PACKAGES}"
+# 创建基础文件系统
+debootstrap \
+    --arch=${ARCH} \
+    --components=main,universe \
+    --include=${INCLUDE_PACKAGES} \
+    ${RELEASE} \
+    ${TARGET_DIR} \
+    ${MIRROR}
+if [ $? -ne 0 ] ; then
+	echo "debootstrap failed!"
+	exit 1
+fi
+
+# 基础系统配置
+chroot ${TARGET_DIR} /bin/bash << EOF
+apt-get update
+apt-get install -y systemd-sysv locales
+locale-gen en_US.UTF-8
+echo root:linux | chpasswd
+rm -rf /var/lib/apt/lists/*
+EOF
+
+echo "All done!"
