6f99fa3843d0faccba3990df501df48c6785d066
yifengyou <842056007@qq.com>
2025-04-23 13:34:49 +0800

    ubuntu18.04 build ok
    Signed-off-by: yifengyou <842056007@qq.com>

--- ubuntu/ubuntu18.04/qcow2/x86_64/build-vm.sh
+++ ubuntu/ubuntu18.04/qcow2/x86_64/build-vm.sh
@@ -2,16 +2,19 @@
 
 set -x
 
-# ISOURL="https://old-releases.ubuntu.com/releases/18.04.4/ubuntu-18.04-server-amd64.iso"
+ISOURL="https://old-releases.ubuntu.com/releases/18.04/ubuntu-18.04.5-live-server-amd64.iso"
+ISOURL="https://old-releases.ubuntu.com/releases/18.04/ubuntu-18.04.5-server-amd64.iso"
 ISOURL="https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cdimage/releases/bionic/release/ubuntu-18.04.6-server-amd64.iso"
-ISONAME=`basename ${ISOURL}`
+FILE_SERVER_PORT="63334"
+VMNAME="kdev-ubuntu18"
+ISONAME=$(basename ${ISOURL})
 
-virsh destroy kdev-ubuntu18 || :
-virsh undefine kdev-ubuntu18 --nvram || :
+virsh destroy ${VMNAME} || :
+virsh undefine ${VMNAME} --nvram || :
 
-if [ -f rootfs.qcow2 ] ; then
+if [ -f rootfs.qcow2 ]; then
 	lsof rootfs.qcow2
-	if [ $? -eq 0 ] ; then
+	if [ $? -eq 0 ]; then
 		echo "rootfs.qcow2 is inuse"
 		exit 1
 	fi
@@ -23,34 +26,40 @@ fi
 
 sync
 
-
-sync
-
 if [ ! -f "${ISONAME}" ]; then
 	wget -c ${ISOURL}
 fi
 
-virt-install --connect qemu:///system \
-	--name kdev-ubuntu18 \
-	--os-variant ubuntu18.04 \
-	--ram 4096 \
-	--vcpus 8 \
-	--graphics none \
+virt-install \
+	--name ${VMNAME} \
+	--arch x86_64 \
+	--vcpus 4 \
+	--ram 2048 \
+	--boot cdrom,hd \
 	--network network=default,model=e1000e \
-	--disk path=rootfs.qcow2,size=8,format=qcow2,bus=scsi,target.dev=sda \
-	--controller type=scsi,model=auto \
-	--location ${ISONAME} \
+	--disk path=$(pwd)/rootfs.qcow2,format=qcow2,bus=scsi \
+	--location $(pwd)/${ISONAME},initrd=install/initrd.gz,kernel=install/vmlinuz \
 	--initrd-inject preseed.cfg \
-	--extra-args="auto=true priority=critical preseed/file=/preseed.cfg console=tty0 console=ttyS0,115200 autoinstall" \
-	--check all=off
+	--extra-args="auto=true priority=critical preseed/file=/preseed.cfg earlyprintk console=tty0 console=ttyS0,115200 " \
+	--graphics none \
+	--console pty,target_type=serial \
+	--os-variant ubuntu18.04 \
+	--check all=off \
+	--debug \
+	--qemu-commandline='-sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny'
 
+echo "virt ret=$?"
 sync
-sleep 3
+
 lsof rootfs.qcow2
-if [ $? -ne 0 ] ; then
-	qemu-img snapshot -c 'install os' rootfs.qcow2
-	qemu-img snapshot -l rootfs.qcow2
+if [ $? -ne 0 ]; then
+	size=$(du -s rootfs.qcow2 | awk '{print $1}')
+	if [ "$size" -gt 204800 ]; then
+		qemu-img snapshot -c 'install os' rootfs.qcow2
+		qemu-img snapshot -l rootfs.qcow2
+	else
+		echo "rootfs.qcow2 size too small, skip snapshot"
+	fi
 fi
 
 exit 0
-
