cec6ce51c2dad5008d57cfcb25e960c5bdb7ddc9
yifengyou <842056007@qq.com>
2025-04-23 01:43:22 +0000

    update ubuntu 24.04 for linux4
    Signed-off-by: yifengyou <842056007@qq.com>

--- ubuntu/ubuntu24.04/qcow2/x86_64/build-vm.sh
+++ ubuntu/ubuntu24.04/qcow2/x86_64/build-vm.sh
@@ -1,44 +1,80 @@
 #!/bin/bash
 
-virsh destroy vm-ubuntu24 || :
-virsh undefine vm-ubuntu24 --nvram || :
+set -x
 
-if [ -f rootfs.qcow2 ];then
-	rm -f rootfs.qcow2
+ISOURL="https://old-releases.ubuntu.com/releases/24.04/ubuntu-24.04-live-server-amd64.iso"
+ISOURL="https://mirrors.aliyun.com/ubuntu-releases/24.04/ubuntu-24.04.2-live-server-amd64.iso"
+FILE_SERVER_PORT="63336"
+ISONAME=`basename ${ISOURL}`
+
+# 检查web文件服务器端口是否被占用
+fileserver=$(lsof -ti :${FILE_SERVER_PORT})
+if [ ! -z "${fileserver}" ] ; then
+  echo "${FILE_SERVER_PORT} already inuse by ${fileserver}"
+  exit 1
 fi
 
-if [ ! -f rootfs.qcow2 ];then
+virsh destroy kdev-ubuntu24 || :
+virsh undefine kdev-ubuntu24 --nvram || :
+
+if [ -f rootfs.qcow2 ] ; then
+	lsof rootfs.qcow2
+	if [ $? -eq 0 ] ; then
+		echo "rootfs.qcow2 is inuse"
+		exit 1
+	fi
+	rm -f rootfs.qcow2
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+else
 	qemu-img create -f qcow2 rootfs.qcow2 500G
 fi
 
 sync
-sleep 1
 
-curl http://192.168.122.1/ks/ubuntu/ &> /dev/null
-if [ $? -ne 0 ] ; then
-	echo "this ubuntu version need webserver to setup automatic installation"
-	exit 1
+if [ ! -f "${ISONAME}" ]; then
+	wget -c ${ISOURL}
 fi
 
-if [ ! -f ubuntu-24.04-live-server-amd64.iso ]; then
-	wget -c https://mirrors.aliyun.com/ubuntu-releases/24.04/ubuntu-24.04.2-live-server-amd64.iso
-fi
+touch meta-data user-data
+
+python3 -m http.server ${FILE_SERVER_PORT} --directory `pwd` &
 
 virt-install \
-	--name vm-ubuntu24 \
-	--os-variant ubuntu20.04 \
-	--ram 4096 \
-	--vcpus 16 \
-	--graphics none \
-	--disk path=rootfs.qcow2,size=8,format=qcow2,bus=scsi,target.dev=sda \
-	--controller type=scsi,model=virtio-scsi \
-	--initrd-inject user-data \
-	--initrd-inject meta-data \
-	--location ubuntu-24.04-live-server-amd64.iso,kernel=casper/vmlinuz,initrd=casper/initrd \
-	--extra-args="priority=critical console=ttyS0,115200 autoinstall ds=nocloud-net;s=http://192.168.122.1/ks/ubuntu/ cloud-config-url=/dev/null  root=/dev/ram0" \
-	--check all=off
+    --name kdev-ubuntu24 \
+    --arch x86_64 \
+    --machine virt-5.2 \
+    --vcpus 4 \
+    --ram 2048 \
+    --cpu host-passthrough \
+    --boot cdrom \
+    --disk path=`pwd`/rootfs.qcow2,format=qcow2,bus=scsi \
+    --location `pwd`/${ISONAME},initrd=casper/initrd,kernel=casper/vmlinuz \
+    --extra-args=" ds=nocloud-net;s=http://192.168.122.1:${FILE_SERVER_PORT} cloud-config-url=/dev/null autoinstall priority=critical console=serial earlyprintk " \
+    --features acpi=on \
+    --graphics none \
+    --console pty,target_type=serial \
+    --os-variant ubuntu20.04 \
+    --check all=off \
+    --debug \
+    --qemu-commandline='-sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny'
 
-qemu-img snapshot -c 'install os' rootfs.qcow2
-qemu-img snapshot -l rootfs.qcow2
+echo "virt ret=$?"
+sync
+
+lsof rootfs.qcow2
+if [ $? -ne 0 ] ; then
+    size=$(du -s rootfs.qcow2 | awk '{print $1}')
+    if [ "$size" -gt 204800 ] ; then
+      qemu-img snapshot -c 'install os' rootfs.qcow2
+      qemu-img snapshot -l rootfs.qcow2
+    else
+      echo "rootfs.qcow2 size too small, skip snapshot"
+    fi
+fi
+
+fileserver=$(lsof -ti :${FILE_SERVER_PORT})
+if [ ! -z "${fileserver}" ] ; then
+  kill -9 ${fileserver}
+fi
 
 exit 0
