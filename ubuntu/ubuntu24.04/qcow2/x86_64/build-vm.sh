8328aac114af11f9bc764831784ee5aa7e5951a2
yifengyou <842056007@qq.com>
2025-04-23 12:15:39 +0800

    update linux5/linux6
    Signed-off-by: yifengyou <842056007@qq.com>

--- ubuntu/ubuntu24.04/qcow2/x86_64/build-vm.sh
+++ ubuntu/ubuntu24.04/qcow2/x86_64/build-vm.sh
@@ -5,21 +5,21 @@ set -x
 ISOURL="https://old-releases.ubuntu.com/releases/24.04/ubuntu-24.04-live-server-amd64.iso"
 ISOURL="https://mirrors.aliyun.com/ubuntu-releases/24.04/ubuntu-24.04.2-live-server-amd64.iso"
 FILE_SERVER_PORT="63336"
-ISONAME=`basename ${ISOURL}`
+VMNAME="kdev-ubuntu24"
+ISONAME=$(basename ${ISOURL})
 
-# 检查web文件服务器端口是否被占用
 fileserver=$(lsof -ti :${FILE_SERVER_PORT})
-if [ ! -z "${fileserver}" ] ; then
-  echo "${FILE_SERVER_PORT} already inuse by ${fileserver}"
-  exit 1
+if [ ! -z "${fileserver}" ]; then
+	echo "${FILE_SERVER_PORT} already inuse by ${fileserver}"
+	exit 1
 fi
 
-virsh destroy kdev-ubuntu24 || :
-virsh undefine kdev-ubuntu24 --nvram || :
+virsh destroy ${VMNAME} || :
+virsh undefine ${VMNAME} --nvram || :
 
-if [ -f rootfs.qcow2 ] ; then
+if [ -f rootfs.qcow2 ]; then
 	lsof rootfs.qcow2
-	if [ $? -eq 0 ] ; then
+	if [ $? -eq 0 ]; then
 		echo "rootfs.qcow2 is inuse"
 		exit 1
 	fi
@@ -37,44 +37,44 @@ fi
 
 touch meta-data user-data
 
-python3 -m http.server ${FILE_SERVER_PORT} --directory `pwd` &
+python3 -m http.server ${FILE_SERVER_PORT} --directory $(pwd) &
+
+cleanup() {
+	fileserver=$(lsof -ti :${FILE_SERVER_PORT})
+	if [ ! -z "${fileserver}" ]; then
+		kill -9 ${fileserver}
+	fi
+}
+trap cleanup EXIT
 
 virt-install \
-    --name kdev-ubuntu24 \
-    --arch x86_64 \
-    --machine virt-5.2 \
-    --vcpus 4 \
-    --ram 2048 \
-    --cpu host-passthrough \
-    --boot cdrom \
-    --disk path=`pwd`/rootfs.qcow2,format=qcow2,bus=scsi \
-    --location `pwd`/${ISONAME},initrd=casper/initrd,kernel=casper/vmlinuz \
-    --extra-args=" ds=nocloud-net;s=http://192.168.122.1:${FILE_SERVER_PORT} cloud-config-url=/dev/null autoinstall priority=critical console=serial earlyprintk " \
-    --features acpi=on \
-    --graphics none \
-    --console pty,target_type=serial \
-    --os-variant ubuntu20.04 \
-    --check all=off \
-    --debug \
-    --qemu-commandline='-sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny'
+	--name ${VMNAME} \
+	--arch x86_64 \
+	--vcpus 4 \
+	--ram 4096 \
+	--boot cdrom,hd \
+	--disk path=$(pwd)/rootfs.qcow2,format=qcow2,bus=scsi \
+	--location $(pwd)/${ISONAME},initrd=casper/initrd,kernel=casper/vmlinuz \
+	--extra-args=" ds=nocloud-net;s=http://192.168.122.1:${FILE_SERVER_PORT} cloud-config-url=/dev/null autoinstall earlyprintk console=ttyS0,115200n8 " \
+	--graphics none \
+	--console pty,target_type=serial \
+	--os-variant ubuntu20.04 \
+	--check all=off \
+	--debug \
+	--qemu-commandline='-sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny'
 
 echo "virt ret=$?"
 sync
 
 lsof rootfs.qcow2
-if [ $? -ne 0 ] ; then
-    size=$(du -s rootfs.qcow2 | awk '{print $1}')
-    if [ "$size" -gt 204800 ] ; then
-      qemu-img snapshot -c 'install os' rootfs.qcow2
-      qemu-img snapshot -l rootfs.qcow2
-    else
-      echo "rootfs.qcow2 size too small, skip snapshot"
-    fi
-fi
-
-fileserver=$(lsof -ti :${FILE_SERVER_PORT})
-if [ ! -z "${fileserver}" ] ; then
-  kill -9 ${fileserver}
+if [ $? -ne 0 ]; then
+	size=$(du -s rootfs.qcow2 | awk '{print $1}')
+	if [ "$size" -gt 204800 ]; then
+		qemu-img snapshot -c 'install os' rootfs.qcow2
+		qemu-img snapshot -l rootfs.qcow2
+	else
+		echo "rootfs.qcow2 size too small, skip snapshot"
+	fi
 fi
 
 exit 0
