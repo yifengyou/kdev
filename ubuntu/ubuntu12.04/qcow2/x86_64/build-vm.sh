4b0bcd81a75f87d28baf09e7066a0c3650490828
yifengyou <842056007@qq.com>
2025-04-23 13:52:30 +0800

    adapt ubuntu12.04 linux2 x86_64
    Signed-off-by: yifengyou <842056007@qq.com>

--- ubuntu/ubuntu12.04/qcow2/x86_64/build-vm.sh
+++ ubuntu/ubuntu12.04/qcow2/x86_64/build-vm.sh
@@ -1,5 +1,72 @@
 #!/bin/bash
 
+set -x
+
+ISOURL="https://old-releases.ubuntu.com/releases/12.04/ubuntu-12.04.5-server-amd64.iso"
+FILE_SERVER_PORT="63332"
+VMNAME="kdev-ubuntu12"
+ISONAME=$(basename ${ISOURL})
+
+virsh destroy ${VMNAME} || :
+virsh undefine ${VMNAME} --nvram || :
+
+if [ -f rootfs.qcow2 ]; then
+	lsof rootfs.qcow2
+	if [ $? -eq 0 ]; then
+		echo "rootfs.qcow2 is inuse"
+		exit 1
+	fi
+	rm -f rootfs.qcow2
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+else
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+fi
+
+sync
+
+if [ ! -f "${ISONAME}" ]; then
+	wget -c ${ISOURL}
+fi
+
+virt-install \
+	--name ${VMNAME} \
+	--arch x86_64 \
+	--vcpus 4 \
+	--ram 2048 \
+	--boot cdrom,hd \
+	--network network=default,model=e1000e \
+	--disk path=$(pwd)/rootfs.qcow2,format=qcow2,bus=sata \
+	--location $(pwd)/${ISONAME},initrd=install/initrd.gz,kernel=install/vmlinuz \
+	--initrd-inject preseed.cfg \
+	--extra-args="auto=true priority=critical preseed/file=/preseed.cfg earlyprintk console=tty0 console=ttyS0,115200 " \
+	--graphics none \
+	--console pty,target_type=serial \
+	--os-variant ubuntu18.04 \
+	--check all=off \
+	--debug \
+	--qemu-commandline='-sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny'
+
+echo "virt ret=$?"
+sync
+
+lsof rootfs.qcow2
+if [ $? -ne 0 ]; then
+	size=$(du -s rootfs.qcow2 | awk '{print $1}')
+	if [ "$size" -gt 204800 ]; then
+		qemu-img snapshot -c 'install os' rootfs.qcow2
+		qemu-img snapshot -l rootfs.qcow2
+	else
+		echo "rootfs.qcow2 size too small, skip snapshot"
+	fi
+fi
+
+exit 0
+
+
+
+
+#!/bin/bash
+
 virsh destroy kdev-ubuntu12 || :
 virsh undefine kdev-ubuntu12 --nvram || :
 
