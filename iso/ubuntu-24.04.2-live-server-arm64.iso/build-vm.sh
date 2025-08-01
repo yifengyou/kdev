ae6cf03f9b1275b7a7d832c76575eef07f3847ee
yifengyou <842056007@qq.com>
2025-08-01 17:37:27 +0800

    add arm64 test
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ iso/ubuntu-24.04.2-live-server-arm64.iso/build-vm.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+ISOURL="https://old-releases.ubuntu.com/releases/24.04/ubuntu-24.04.1-live-server-amd64.iso"
+ISOURL="https://mirrors.aliyun.com/ubuntu-releases/24.04/ubuntu-24.04.1-live-server-arm64.iso"
+ISOURL="https://releases.ubuntu.com/24.04/ubuntu-24.04.1-live-server-arm64.iso"
+
+WORKDIR=`pwd`
+FILE_SERVER_PORT="63336"
+VMNAME="kdev-$RANDOM"
+ISONAME=$(basename ${ISOURL})
+LOGNAME="kdev.log"
+
+
+sudo apt-get install -y qemu-efi-aarch64 qemu-utils \
+	ipxe-qemu
+
+
+fileserver=$(lsof -ti :${FILE_SERVER_PORT} || :)
+if [ ! -z "${fileserver}" ]; then
+	echo "${FILE_SERVER_PORT} already inuse by ${fileserver}"
+	exit 1
+fi
+
+if [ -f rootfs.qcow2 ]; then
+	lsof rootfs.qcow2
+	if [ $? -eq 0 ]; then
+		echo "rootfs.qcow2 is inuse"
+		exit 1
+	fi
+	rm -f rootfs.qcow2
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+else
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+fi
+echo "kdev: rootfs.qcow2 ready!"
+
+sync
+
+if [ ! -f "${ISONAME}" ]; then
+	aria2c ${ISOURL}
+	if [ $? -ne 0 ] ; then
+		echo "kdev: download ${ISONAME} failed!"
+		exit 1
+	fi
+fi
+echo "kdev: ${ISONAME} ready!"
+
+touch meta-data user-data
+
+python3 -m http.server ${FILE_SERVER_PORT} --directory $(pwd) &
+echo "kdev: http server ready!"
+
+cleanup() {
+	fileserver=$(lsof -ti :${FILE_SERVER_PORT})
+	if [ ! -z "${fileserver}" ]; then
+		kill -9 ${fileserver}
+	fi
+	umount -l mnt
+
+}
+trap cleanup EXIT
+
+mkdir -p mnt
+mount ${ISONAME} mnt 
+if [ $? -ne 0 ]; then
+	echo "mount ${ISONAME} failed!"
+	exit 1
+fi
+
+ls -alh mnt/casper
+if [ $? -ne 0 ]; then
+	echo "casper does't exists in ${ISONAME}"
+	exit 1
+fi
+
+set -x
+qemu-system-aarch64 \
+  -name kdev-ubuntu2404 \
+  -machine virt \
+  -cpu max \
+  -drive file=/usr/share/AAVMF/AAVMF_CODE.fd,format=raw,if=pflash \
+  -smp 2 \
+  -m 2048 \
+  -cdrom ${ISONAME} \
+  -device virtio-scsi-pci,id=scsi \
+  -drive file=/root/rootfs.qcow2,format=qcow2,if=virtio \
+  -boot order=dc \
+  -kernel /root/mnt/casper/vmlinuz \
+  -initrd /root/mnt/casper/initrd \
+  -append 'ds=nocloud-net;s=http://192.168.122.1:63336/ cloud-config-url=/dev/null autoinstall earlyprintk console=ttyS0,115200n8' \
+  -serial file:kdev.log \
+  -net nic \
+  -net user,net=192.168.122.0/24,host=192.168.122.1 \
+  -display curses 
+#  --daemonize
+
+if [ $? -ne 0 ] ; then
+	echo "qemu exit with error"
+	exit 1
+fi
+
+tail -f ${LOGNAME} &
+
+while true ; do
+	ps aux|grep qemu-system-x86_64 |grep -v grep &> /dev/null
+	if [ $? -ne 0 ] ; then
+		break
+	fi
+	sleep 3
+done
+
+sync
+
+lsof rootfs.qcow2
+if [ $? -ne 0 ]; then
+	size=$(du -s rootfs.qcow2 | awk '{print $1}')
+	if [ "$size" -gt 204800 ]; then
+		qemu-img snapshot -c 'install os' rootfs.qcow2
+		qemu-img snapshot -l rootfs.qcow2
+		ls -alh rootfs.qcow2
+	else
+		echo "rootfs.qcow2 size too small, skip snapshot"
+	fi
+fi
+
+umount mnt -l || true
+rmdir mnt || true
+
+exit 0
+
