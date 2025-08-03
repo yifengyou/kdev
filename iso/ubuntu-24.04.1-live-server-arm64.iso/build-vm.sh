6ba3db71f09cb48d5bcd5ea26cbf0c2368b45314
yifengyou <842056007@qq.com>
2025-08-03 10:58:03 +0800

    try fix build error
    root        5219  0.0  0.0   6416  1920 ?        S    22:14   0:00 grep tmux
    kdev: 1 windows (created Sat Aug  2 22:13:44 2025)
    3hEFI stub: Booting Linux Kernel...
    EFI stub: EFI_RNG_PROTOCOL unavailable
    EFI stub: Loaded initrd from LINUX_EFI_INITRD_MEDIA_GUID device path
    EFI stub: Generating empty DTB
    EFI stub: Exiting boot services...
    
    Signed-off-by: yifengyou <842056007@qq.com>

--- iso/ubuntu-24.04.1-live-server-arm64.iso/build-vm.sh
+++ iso/ubuntu-24.04.1-live-server-arm64.iso/build-vm.sh
@@ -105,7 +105,7 @@ tmux new-session -d -s kdev \
   -boot order=dc \
   -kernel mnt/casper/vmlinuz \
   -initrd mnt/casper/initrd \
-  -append 'ds=nocloud-net;s=http://192.168.122.1:63336/ cloud-config-url=/dev/null autoinstall earlyprintk console=ttyS0,115200n8  earlycon=uart,mmio,0x9000000 ignore_loglevel console=ttyAMA0,115200n8' \
+  -append 'ds=nocloud-net;s=http://192.168.122.1:63336/ cloud-config-url=/dev/null autoinstall earlyprintk ignore_loglevel console=ttyAMA0,115200n8 earlycon=pl011,mmio,0x09000000' \
   -serial file:kdev.log \
   -net nic \
   -net user,net=192.168.122.0/24,host=192.168.122.1 \
