72c8a7393d2389d14d1298221bba2d17eb034722
yifengyou <842056007@qq.com>
2025-08-03 20:53:08 +0800

    disable casper md5sum service
    Signed-off-by: yifengyou <842056007@qq.com>

--- iso/ubuntu-24.04.1-live-server-arm64.iso/build-vm.sh
+++ iso/ubuntu-24.04.1-live-server-arm64.iso/build-vm.sh
@@ -106,7 +106,7 @@ sudo qemu-system-aarch64 \
   -boot order=dc \
   -kernel mnt/casper/vmlinuz \
   -initrd mnt/casper/initrd \
-  -append 'ds=nocloud-net;s=http://192.168.122.1:63336/ cloud-config-url=/dev/null autoinstall earlyprintk ignore_loglevel console=ttyAMA0,115200n8 earlycon=pl011,mmio,0x09000000 level=10 systemd.mask=snapd.service systemd.mask=snapd.seeded.service ' \
+  -append 'ds=nocloud-net;s=http://192.168.122.1:63336/ cloud-config-url=/dev/null autoinstall earlyprintk ignore_loglevel console=ttyAMA0,115200n8 earlycon=pl011,mmio,0x09000000 level=10 systemd.mask=snapd.service systemd.mask=snapd.seeded.service systemd.mask=casper-md5check.service ' \
   -serial mon:stdio \
   -net nic \
   -net user,net=192.168.122.0/24,host=192.168.122.1 \
