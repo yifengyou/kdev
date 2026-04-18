1b0ebeb7f6be52007b54fc003109555c3f03f6b0
yifengyou <842056007@qq.com>
2026-04-18 10:49:10 +0800

    测试rocky镜像，可能因为指令集不支持导致crash
    !!!! X64 Exception Type - 06(#UD - Invalid Opcode)  CPU Apic ID - 00000000 !!!!
    RIP  - 0000000072BF7CE8, CS  - 0000000000000038, RFLAGS - 0000000000010202
    RAX  - 000000007FE65701, RCX - 000000007F20AF60, RDX - 000000007FE65700
    RBX  - 000000007D834298, RSP - 000000007FE65870, RBP - 000000007FE658FC
    RSI  - 000000007D834298, RDI - 000000007FE65878
    R8   - 000000007FE657D8, R9  - 0000000000000000, R10 - 0000000000000028
    R11  - 0000000000000000, R12 - 000000007F7EC001, R13 - 0000000000000000
    R14  - 0000000000000000, R15 - 0000000000000000
    DS   - 0000000000000030, ES  - 0000000000000030, FS  - 0000000000000030
    GS   - 0000000000000030, SS  - 0000000000000030
    CR0  - 0000000080010033, CR2 - 0000000000000000, CR3 - 000000007FA01000
    CR4  - 0000000000000668, CR8 - 0000000000000000
    DR0  - 0000000000000000, DR1 - 0000000000000000, DR2 - 0000000000000000
    DR3  - 0000000000000000, DR6 - 00000000FFFF0FF0, DR7 - 0000000000000400
    GDTR - 000000007F7DC000 0000000000000047, LDTR - 0000000000000000
    IDTR - 000000007F22E018 0000000000000FFF,   TR - 0000000000000000
    FXSAVE_STATE - 000000007FE654D0
    !!!! Find image based on IP(0x72BF7CE8) (No PDB)  (ImageBase=0000000071CE9000, EntryPoint=0000000072BFD657) !!!!
    
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ iso/Rocky-10.0-x86_64-minimal/03.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+ISOURL="https://dl.rockylinux.org/vault/rocky/10.0/isos/x86_64/Rocky-10.0-x86_64-minimal.iso"
+WORKDIR=$(pwd)
+FILE_SERVER_PORT=$(shuf -i 20000-65535 -n 1)
+ISONAME=$(basename ${ISOURL})
+JOBS=$(nproc)
+
+if [ "$(id -u)" != "0" ]; then
+	echo "must run as root"
+	exit 1
+fi
+
+PKGS=(
+	"tmux"
+	"qemu-system-arm"
+	"qemu-system-gui"
+	"qemu-utils"
+	"ipxe-qemu"
+	"libvirt-daemon-system"
+	"virtinst"
+	"cpu-checker"
+	"aria2"
+)
+MISSING=()
+
+for p in "${PKGS[@]}"; do
+	if ! dpkg -s "$p" &>/dev/null; then
+		MISSING+=("$p")
+		echo "❌ 缺失: $p"
+	else
+		echo "✅ 已有: $p"
+	fi
+done
+
+if [ ${#MISSING[@]} -gt 0 ]; then
+	echo "--------------------------------"
+	sudo apt-get update && sudo apt-get install -y "${MISSING[@]}"
+else
+	echo "--------------------------------"
+	echo "✨ 所有软件包均已安装。"
+fi
+
+fileserver=$(lsof -ti :${FILE_SERVER_PORT})
+if [ ! -z "${fileserver}" ]; then
+	kill -9 ${fileserver}
+fi
+
+if [ -f rootfs.qcow2 ]; then
+	size=$(du -s rootfs.qcow2 | awk '{print $1}')
+	if [ "$size" -gt 204800 ]; then
+		echo "rootfs.qcow2 already ok!"
+		ls -alh rootfs.qcow2
+		exit 0
+	fi
+	lsof rootfs.qcow2
+	if [ $? -eq 0 ]; then
+		echo "kdev: rootfs.qcow2 is inuse"
+		exit 1
+	fi
+	rm -f rootfs.qcow2
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+else
+	qemu-img create -f qcow2 rootfs.qcow2 500G
+fi
+echo "kdev: rootfs.qcow2 ready!"
+
+sync
+
+if [ ! -f "${ISONAME}" ]; then
+	which aria2c
+	if [ $? -eq 0 ]; then
+		aria2c --max-tries=10 --retry-wait=5 ${ISOURL}
+	fi
+fi
+
+if [ ! -f "${ISONAME}" ]; then
+	which wget
+	if [ $? -eq 0 ]; then
+		wget -c ${ISOURL}
+	fi
+fi
+
+if [ ! -f "${ISONAME}" ]; then
+	which curl
+	if [ $? -eq 0 ]; then
+		curl -o ${ISONAME} ${ISOURL}
+	fi
+fi
+
+if [ ! -f "${ISONAME}" ]; then
+	echo "kdev: cound't download ${ISONAME}"
+	exit 1
+fi
+
+ls -alh ${ISONAME}
+echo "kdev: ${ISONAME} ready!"
+
+python3 -m http.server ${FILE_SERVER_PORT} --directory $(pwd) &
+echo "kdev: http server ready!"
+
+cleanup() {
+	fileserver=$(lsof -ti :${FILE_SERVER_PORT})
+	if [ ! -z "${fileserver}" ]; then
+		kill -9 ${fileserver}
+	fi
+	umount -l mnt
+	rmdir mnt
+}
+trap cleanup EXIT
+
+mkdir -p mnt
+mount ${ISONAME} mnt
+if [ $? -ne 0 ]; then
+	echo "kdev: mount ${ISONAME} failed!"
+	exit 1
+fi
+
+if [ ! -f mnt/images/pxeboot/vmlinuz ]; then
+	echo "casper/vmlinuz does't exists!"
+	exit 1
+fi
+
+if [ ! -f mnt/images/pxeboot/initrd.img ]; then
+	echo "casper/initrd does't exists!"
+	exit 1
+fi
+
+qemu-system-x86_64 \
+	-name "${ISONAME%.*}" \
+	-machine q35,accel=kvm \
+	-cpu host \
+	-smp 4 \
+	-m 2048 \
+	-drive if=pflash,format=raw,unit=0,file=/usr/share/edk2/ovmf/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,unit=1,file=/usr/share/edk2/ovmf/OVMF_VARS.fd \
+	-cdrom ${ISONAME} \
+	-device virtio-scsi-pci,id=scsi \
+	-drive file=rootfs.qcow2,format=qcow2,if=virtio \
+	-kernel mnt/images/pxeboot/vmlinuz \
+	-initrd mnt/images/pxeboot/initrd.img \
+	-append "inst.ks=http://192.168.122.1:${FILE_SERVER_PORT}/ks.cfg inst.stage2=hd:LABEL=Rocky-10-0-x86_64-dvd inst.text inst.cmdline earlyprintk ignore_loglevel console=tty0 level=10 net.ifnames=0 biosdevname=0 " \
+	-serial mon:stdio \
+	-net nic \
+	-net user,net=192.168.122.0/24,host=192.168.122.1 \
+	-nographic
+
+sync
+ls -alh rootfs.qcow2
+size=$(du -s rootfs.qcow2 | awk '{print $1}')
+if [ "$size" -gt 204800 ]; then
+	qemu-img convert -c -O qcow2 rootfs.qcow2 rootfs.qcow2.tmp
+	qemu-img check rootfs.qcow2.tmp
+	mv rootfs.qcow2.tmp rootfs.qcow2
+	ls -alh rootfs.qcow2
+	qemu-img snapshot -c 'install os' rootfs.qcow2
+	qemu-img snapshot -l rootfs.qcow2
+	ls -alh rootfs.qcow2
+else
+	echo "kdev: rootfs.qcow2 size too small, abort!"
+	exit 1
+fi
+
+echo "kdev: all done!"
+exit 0
