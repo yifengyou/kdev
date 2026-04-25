d421e21f787d45310e5eb9456caf64f0fceace1f
yifengyou <842056007@qq.com>
2026-04-25 16:16:28 +0000

    解决构建错误
    + chown -R kdev:kdev
    chown: missing operand after ‘kdev:kdev’
    Try 'chown --help' for more information.
    Error: Process completed with exit code 1.
    
    Signed-off-by: yifengyou <842056007@qq.com>

--- kdevbuild/do.sh
+++ kdevbuild/do.sh
@@ -2,11 +2,12 @@
 
 set -xe
 
+WORKDIR=$(pwd)
 sudo yum makecache
 sudo yum install -y git make wget tar gcc
 
+cd ${WORKDIR}
 MUSL_VERSION="1.2.5"
-
 wget https://www.musl-libc.org/releases/musl-$MUSL_VERSION.tar.gz
 tar -xzf musl-$MUSL_VERSION.tar.gz
 cd musl-$MUSL_VERSION
@@ -17,9 +18,10 @@ sudo make install
 export PATH="/usr/local/musl/bin:${PATH}"
 echo 'export PATH="/usr/local/musl/bin:${PATH}"' >> ~/.bashrc
 
-git clone https://github.com/Notselwyn/CVE-2024-1086.git $CVE_DIR
-chown -R kdev:kdev $CVE_DIR
-cd $CVE_DIR
+cd ${WORKDIR}
+git clone https://github.com/Notselwyn/CVE-2024-1086.git CVE-2024-1086.git
+chown -R kdev:kdev CVE-2024-1086.git
+cd CVE-2024-1086.git
 sudo make -j$(nproc)
 
 ls -alh exploit
