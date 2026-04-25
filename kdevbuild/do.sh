47c76fba7e8218deabf3c77d3ec64844a4a5c2f5
yifengyou <842056007@qq.com>
2026-04-25 16:10:16 +0000

    解决构建错误
    Signed-off-by: yifengyou <842056007@qq.com>

--- kdevbuild/do.sh
+++ kdevbuild/do.sh
@@ -6,10 +6,6 @@ sudo yum makecache
 sudo yum install -y git make wget tar gcc
 
 MUSL_VERSION="1.2.5"
-MUSL_ROOT="/opt/app/musl"
-
-mkdir -p $MUSL_ROOT
-cd $MUSL_ROOT
 
 wget https://www.musl-libc.org/releases/musl-$MUSL_VERSION.tar.gz
 tar -xzf musl-$MUSL_VERSION.tar.gz
@@ -18,21 +14,13 @@ cd musl-$MUSL_VERSION
 make
 sudo make install
 
-cd /opt/app
-sudo rm -rf $MUSL_ROOT
-
 export PATH="/usr/local/musl/bin:${PATH}"
 echo 'export PATH="/usr/local/musl/bin:${PATH}"' >> ~/.bashrc
-
-APP_ROOT="/opt/app"
-CVE_DIR="$APP_ROOT/CVE-2024-1086"
-
-mkdir -p $APP_ROOT
 
 git clone https://github.com/Notselwyn/CVE-2024-1086.git $CVE_DIR
 chown -R kdev:kdev $CVE_DIR
 cd $CVE_DIR
-make -j$(nproc)
+sudo make -j$(nproc)
 
 ls -alh exploit
 chmod 755 exploit
