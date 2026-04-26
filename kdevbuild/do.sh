7a59ac87f12aa34d181e9daa5685deaab07bd65f
yifengyou <842056007@qq.com>
2026-04-26 00:55:16 +0000

    解决sudo环境变量问题
    Signed-off-by: yifengyou <842056007@qq.com>

--- kdevbuild/do.sh
+++ kdevbuild/do.sh
@@ -22,7 +22,7 @@ cd ${WORKDIR}
 git clone https://github.com/Notselwyn/CVE-2024-1086.git CVE-2024-1086.git
 chown -R kdev:kdev CVE-2024-1086.git
 cd CVE-2024-1086.git
-sudo make -j$(nproc)
+sudo -E make -j$(nproc)
 
 ls -alh exploit
 chmod 755 exploit
