0641c7099607668fe6e8568612abe8b275d2151e
yifengyou <842056007@qq.com>
2026-05-01 13:26:34 +0000

    解决debian amd64 efi模式无人值守安装问题
    Signed-off-by: yifengyou <842056007@qq.com>

--- iso/debian-12.13.0-amd64-DVD-1/build-vm.sh
+++ iso/debian-12.13.0-amd64-DVD-1/build-vm.sh
@@ -118,7 +118,8 @@ qemu-system-x86_64 \
   -drive if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE.fd \
   -drive if=pflash,format=raw,unit=1,file=/usr/share/OVMF/OVMF_VARS.fd \
   -cdrom "${ISONAME}" \
-  -hda rootfs.qcow2 \
+  -device virtio-scsi-pci,id=scsi \
+  -drive file=rootfs.qcow2,format=qcow2,if=virtio \
   -boot order=dc \
   -kernel mnt/install.amd/vmlinuz \
   -initrd mnt/install.amd/initrd.gz \
