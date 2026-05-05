5f2bf48f7fbcb12b4ee7765ef204b639576da147
yifengyou <842056007@qq.com>
2026-05-05 02:50:12 +0000

    添加更多ubuntu 18.04 amd64支持
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ iso/ubuntu-18.04.3-server-amd64/run-vm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+WORKDIR=$(pwd)
+LOCALPORT=$(shuf -i 20000-65535 -n 1)
+
+if [ ! -f rootfs.qcow2 ]; then
+	echo "kdev: no rootfs found!"
+	exit 1
+fi
+
+if [ "$(id -u)" != "0" ]; then
+	echo "kdev: must run as root"
+	exit 1
+fi
+
+set -x
+
+qemu-system-x86_64 \
+	-name "kdev-${RANDOM}" \
+	-machine q35,accel=kvm \
+	-drive if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,unit=1,file=/usr/share/OVMF/OVMF_VARS.fd \
+	-cpu max \
+	-smp $(nproc) \
+	-m 4096 \
+	-device virtio-scsi-pci,id=scsi \
+	-drive file=rootfs.qcow2,format=qcow2,if=virtio \
+	-net nic \
+	-net user,net=192.168.122.0/24,host=192.168.122.1,hostfwd=tcp::${LOCALPORT}-:22 \
+	-nographic \
+	-serial mon:stdio
+
+echo "kdev: all done!"
+exit 0
+
