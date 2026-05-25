e3f4726e1cf3a982f6016653115cff45733690fc
yifengyou <842056007@qq.com>
2026-05-25 10:20:34 +0000

    更新ubuntu 24.04 qcow2运行脚本
    Signed-off-by: yifengyou <842056007@qq.com>

--- iso/ubuntu-24.04.2-live-server-amd64/run-vm.sh
+++ iso/ubuntu-24.04.2-live-server-amd64/run-vm.sh
@@ -16,22 +16,22 @@ fi
 
 set -x
 
-qemu-system-aarch64 \
+qemu-system-x86_64 \
 	-name "kdev-${RANDOM}" \
-	-machine virt \
-	-cpu max \
+	-machine q35 \
 	-accel kvm \
-	-semihosting \
-	-drive file=/usr/share/AAVMF/AAVMF_CODE.fd,format=raw,if=pflash \
+	-drive if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,unit=1,file=/usr/share/OVMF/OVMF_VARS.fd \
+	-cpu max \
 	-smp 4 \
 	-m 4096 \
 	-device virtio-scsi-pci,id=scsi \
 	-drive file=rootfs.qcow2,format=qcow2,if=virtio \
-	-boot order=hd \
-	-serial mon:stdio \
 	-net nic \
 	-net user,net=192.168.122.0/24,host=192.168.122.1,hostfwd=tcp::${LOCALPORT}-:22 \
-	-nographic
+	-nographic \
+	-serial mon:stdio
 
 echo "kdev: all done!"
 exit 0
+
